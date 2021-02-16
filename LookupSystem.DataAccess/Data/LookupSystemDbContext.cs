d516c8e6a52038af3a47b03e2c23e012fef1a8d8
vms <vmshchuruk.dev@gmail.com>
2021-02-16 17:28:13 +0200

    Added projects and database

---
+++ LookupSystem.DataAccess/Data/LookupSystemDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LookupSystemService.Models;
+
+namespace LookupSystem.DataAccess.Data
+{
+    public class LookupSystemDbContext : DbContext
+    {
+        public LookupSystemDbContext(DbContextOptions<LookupSystemDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserContact> UserContacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.UserContact)
+                .WithOne(uc => uc.User)
+                .HasForeignKey<UserContact>(fk => fk.UserId);
+
+            //modelBuilder.Entity<User>();
+            //modelBuilder.Entity<UserContact>();
+        }
+    }
+}
---
+++ LookupSystemService/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LookupSystemService.Models
+{
+    public class UserViewModel
+    {
+    }
+}
