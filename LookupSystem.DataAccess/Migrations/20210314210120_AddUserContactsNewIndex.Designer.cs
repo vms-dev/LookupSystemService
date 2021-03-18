626bd2673016f0c9046fe4292c16ba0beaa63239
vms <vmshchuruk.dev@gmail.com>
2021-03-18 18:49:07 +0200

    Update Indexes

--- LookupSystem.DataAccess/Migrations/20210314210120_AddUserContactsNewIndex.Designer.cs
+++ LookupSystem.DataAccess/Migrations/20210314210120_AddUserContactsNewIndex.Designer.cs
@@ -4,14 +4,16 @@ using LookupSystem.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LookupSystem.DataAccess.Migrations
 {
     [DbContext(typeof(LookupSystemDbContext))]
-    partial class LookupSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210314210120_AddUserContactsNewIndex")]
+    partial class AddUserContactsNewIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
